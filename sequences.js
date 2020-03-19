30ef7acc0703b97150a8540b3e32f57508aa9a79
PavloMartovichPowerSnow <mryaremchyk@gmail.com>
2020-03-19 22:40:24 +0200

    Code

--- sequences.js
+++ sequences.js
@@ -1,3 +1,5 @@
+const firstElementOfAllSequences = 'n'+'   '+'a(n)'+'\n';
+const spaces = '   ';
 const sequenceName = document.querySelector('.stype');
 const lastElement = document.querySelector('.laste');
 const sequenceOutput = document.querySelector('.out_result');
@@ -5,7 +7,8 @@ const submitButton = document.querySelector('.submit').addEventListener('click',
 
 
 function prime(n){
-	let seq=[0,1], div=0, count=0;
+	const seq=[];
+	let div=0, count=0;
 	for(let i=0; i<n;++i){
 		div=1;
 		while(div<=parseInt(i/2)){
@@ -19,10 +22,16 @@ function prime(n){
 		}
 		count=0;
 	}
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i + 1, spaces, seq[i], '\n');
+	}
+	return newSeq;
 }
 function nonprime(n){
-	let seq=[], div=0, count=0;
+	const seq=[]; 
+	let div=0, count=0;
 	for(let i=0; i<n;++i){
 		div=1;
 		while(div<=parseInt(i/2)){
@@ -36,10 +45,16 @@ function nonprime(n){
 		}
 		count=0;
 	}
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i + 1, spaces, seq[i], '\n');
+	}
+	return newSeq;
 }
 function phi(n){
-	let div=0, count=0, flag=0, seq=[];
+	const seq=[];
+	let div=0, count=0, flag=0;
 
 	for(let i=1; i<=n; ++i){
 		count=0;
@@ -58,10 +73,16 @@ function phi(n){
 		seq.push(count);
 	}
 	seq[0]=1;
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i + 1, spaces, seq[i], '\n');
+	}
+	return newSeq;
 }
 function recaman(n){
-	let seq=[];
+	const seq=[];
+	let indexOfElement = 0;
 	for(let i=0; i<=n; ++i){
 		if(i==0){
 			seq.push(i);
@@ -72,24 +93,40 @@ function recaman(n){
 		else{
 			seq.push(seq[i-1]+i);
 		}
+		++indexOfElement;
 	}
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i, spaces, seq[i], '\n');
+	}
+	return newSeq;
 }
 function fibonacci(n){
-	let seq=[0,1];
+	const seq = [0, 1];
+	let first = 0, second = 1, result = 0;
 	if(n==0){
-		return seq[0];
+		return seq.slice(0,4);
 	}
 	if(n==1){
 		return seq;
 	}
 	for(let i=2;i<=n;++i){
-		seq.push(seq[i-2]+seq[i-1]);
+		result = first + second;
+		seq.push(result);
+		first = second;
+		second = result;
 	}
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i, spaces, seq[i], '\n');
+	}
+	return newSeq;
 }
 function perfect(n){
-	let seq=[], div=0, sum=0;
+	const seq=[];
+	let div=0, sum=0;
 	for(let i=1; i<n; ++i){
 		div=1;
 		while(div<=parseInt(i/2)){
@@ -103,10 +140,16 @@ function perfect(n){
 		 } 
 		 sum=0;
 	}
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i + 1, spaces, seq[i], '\n');
+	}
+	return newSeq;
 }
 function opmanis(n){
-	let seq=[], buf='0', ax=0, flag=0;
+	const seq=[];
+	let buf='0', ax=0, flag=0;
 		for(let i=1; i<=n;++i){
 			ax=1; flag=0; buf='0';
 			while(ax<=i){
@@ -121,22 +164,93 @@ function opmanis(n){
 				seq.push(0);
 			}
 		}
-	return seq;
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i + 1, spaces, seq[i], '\n');
+	}
+	return newSeq;
+}
+function factorial(n) {
+	let result = 1;
+	let multiplicator = n;
+	while(multiplicator>1) {
+		result = result*multiplicator;
+		--multiplicator;
+	}
+	return result;
+}
+function catalan(n) {
+	const seq=[];
+	for(let i=0; i<=n; ++i) {
+		seq.push(Math.round(factorial(2*i)/(factorial(i)*factorial(i+1))));
+	}
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences);
+	for(let i = 0; i<seq.length; ++i) {
+		newSeq.push(i, spaces, seq[i], '\n');
+	}
+	return newSeq;
+}
+function bin(n) {
+	const seq = [];
+	let buf = '', ax = 0;
+	for(let i = 0; i<=n; ++i) {
+		ax = i;
+		while(ax>0) {
+			buf += String(ax%2);
+			ax = parseInt(ax/2);
+		}
+		seq.push(buf.split('').reverse().join());
+		buf = '';
+	}
+	const newSeq = [];
+	newSeq.push(firstElementOfAllSequences, 0, spaces, -1, '\n');
+	for(let i = 1; i<seq.length; ++i) {
+		newSeq.push(
+			i, spaces,
+			String(seq[i]).replace(/0/g,'').length - String(seq[i]).replace(/1/g,'').length,
+			'\n'
+			);
+	}
+	return newSeq;
 }
 
 function calculate() {
     if(lastElement.value === '') {
-        alert("You should set last element!");
-    }
+		alert("You should set last element!");
+		return;
+	}
+	else {
+		lastElement.value = parseInt(lastElement.value);
+	}
     switch(sequenceName.value) {
         case "prime":
-            sequenceOutput.innerHTML = prime(parseInt(lastElement.value));
+            sequenceOutput.innerHTML = String(prime(lastElement.value)).replace(/,/g, '');
             break;
         case "nonprime":
-            sequenceOutput.innerHTML = nonprime(lastElement.value);
+            sequenceOutput.innerHTML = String(nonprime(lastElement.value)).replace(/,/g, '');
             break;
         case "fibonacci":
-            sequenceOutput.innerHTML = fibonacci(lastElement.value);
-            break;
+            sequenceOutput.innerHTML = String(fibonacci(lastElement.value)).replace(/,/g, '');
+			break;
+		case "recaman":
+			sequenceOutput.innerHTML = String(recaman(lastElement.value)).replace(/,/g, '');
+			break;
+		case "catalan":
+			sequenceOutput.innerHTML = String(catalan(lastElement.value)).replace(/,/g, '');
+			break;
+		case "totient":
+			sequenceOutput.innerHTML = String(phi(lastElement.value)).replace(/,/g, '');
+			break;
+		case "perfect":
+			sequenceOutput.innerHTML = String(perfect(lastElement.value)).replace(/,/g, '');
+			break;
+		case "opmanis":
+			sequenceOutput.innerHTML = String(opmanis(lastElement.value)).replace(/,/g, '');
+			break;
+		case "binary":
+			sequenceOutput.innerHTML = String(bin(lastElement.value)).replace(/,/g, '');
+			break;	
     }
 }
