14ae1c421ce7bd02744f4bb9599a3ce8f968dea2
PavloMartovichPowerSnow <mryaremchyk@gmail.com>
2020-03-20 14:45:42 +0200

    Comment

--- matrix.js
+++ matrix.js
@@ -5,7 +5,7 @@ const inputField = document.querySelector('.input_field');
 const confirm = document.querySelector('.confirm').addEventListener('click', setMtx);
 const result = document.querySelector('.get_res').addEventListener('click', calculate);
 
-function createMatrix(label = '' ,rows, columns) {
+function createMatrix(label ,rows, columns) {
     const capt = document.createElement('caption');
     capt.innerHTML = "Input matrix "+label;
     inputField.appendChild(capt);
@@ -31,13 +31,59 @@ function setMtx() {
         alert("Numbers of rows and columns can't be equal zero!");
         return;
     }
+    inputField.innerHTML = '';
     switch(operation.value) {
         case "multiplicate":
             createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             createMatrix('B', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
+        case "add":
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('B', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            break;
+        case "range":
+            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            break;
+        case "determinant":
+            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            break;
+        case "subtract":
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('B', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            break;
+        case "power":
+            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            const powerLable = document.createElement('lable');
+            powerLable.innerHTML = "Degree: ";
+            powerLable.style.display = 'inline-block';
+            powerLable.style.marginTop = '15px';
+            const powerInput = document.createElement('input');
+            powerInput.setAttribute('type', 'number');
+            powerInput.setAttribute('class', 'degree_value');
+            powerInput.style.width = '45px';
+            powerLable.appendChild(powerInput);
+            inputField.appendChild(powerLable);
+            break;
+        case "transpone":
+            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            break;
+        case "of num":
+            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            const numLable = document.createElement('lable');
+            numLable.innerHTML = "Number: ";
+            numLable.style.display = 'inline-block';
+            numLable.style.marginTop = '15px';
+            const numberInput = document.createElement('input');
+            numberInput.setAttribute('type', 'number');
+            numberInput.setAttribute('class', 'number_value');
+            numberInput.style.width = '45px';
+            numLable.appendChild(numberInput);
+            inputField.appendChild(numLable);
+            break;
+        case "convert":
+            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            break;
     }
-    
 }
 
 function calculate() {
