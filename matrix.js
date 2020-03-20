a7a7a97c0f7ffc5de6590de258436c1b72c946d9
PavloMartovichPowerSnow <mryaremchyk@gmail.com>
2020-03-20 16:20:41 +0200

    Comment

--- matrix.js
+++ matrix.js
@@ -14,7 +14,6 @@ function createMatrix(label ,rows, columns) {
         for(let j=0; j<columns; ++j) {
             row = document.createElement('input');
             row.setAttribute('type', 'number');
-            row.style.width = '45px';
             row.setAttribute('class', label+String(i)+String(j));
             inputField.appendChild(row);
         }
@@ -42,50 +41,58 @@ function setMtx() {
             createMatrix('B', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
         case "range":
-            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
         case "determinant":
-            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
         case "subtract":
             createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             createMatrix('B', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
         case "power":
-            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             const powerLable = document.createElement('lable');
+            powerLable.setAttribute('class', 'lable_of_')
             powerLable.innerHTML = "Degree: ";
-            powerLable.style.display = 'inline-block';
-            powerLable.style.marginTop = '15px';
             const powerInput = document.createElement('input');
             powerInput.setAttribute('type', 'number');
             powerInput.setAttribute('class', 'degree_value');
-            powerInput.style.width = '45px';
             powerLable.appendChild(powerInput);
             inputField.appendChild(powerLable);
             break;
         case "transpone":
-            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
         case "of num":
-            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             const numLable = document.createElement('lable');
+            numLable.setAttribute('class', 'lable_of_');
             numLable.innerHTML = "Number: ";
-            numLable.style.display = 'inline-block';
-            numLable.style.marginTop = '15px';
             const numberInput = document.createElement('input');
             numberInput.setAttribute('type', 'number');
             numberInput.setAttribute('class', 'number_value');
-            numberInput.style.width = '45px';
             numLable.appendChild(numberInput);
             inputField.appendChild(numLable);
             break;
         case "convert":
-            createMatrix('', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
+            createMatrix('A', parseInt(numberOfRows.value), parseInt(numberOfColumns.value));
             break;
     }
 }
 
+function multiplicateOnNumber() {
+    const mlt = parseFloat(document.querySelector('.number_value').value);
+    const arr = [];
+    for(let i=0; i<parseInt(numberOfRows.value); ++i) {
+        for(let j=0; j<parseInt(numberOfColumns.value); ++j) {
+            arr.push(mlt * parseFloat(document
+                .querySelector('.' + 'A' +String(i) + String(j)).value));
+        }
+    }
+    return arr;
+}
+
 function calculate() {
-    alert(document.querySelector('.A00').value);
+    alert(multiplicateOnNumber());
 }
